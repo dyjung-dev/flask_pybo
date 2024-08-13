a8caf7aa0b9f5999cbb87c02a84fdcd98491e515
dyjung.dev <dyjung.dev@gmail.com>
2024-08-13 17:53:04 +0900

    commit 0813

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -17,10 +17,13 @@ def create_app():
     #ORM
     db.init_app(app)
     migrate.init_app(app, db)
+    from . import models
 
 
     #Blueprint
-    from .views import main_views
+    from .views import main_views, question_views,answer_views
     app.register_blueprint(main_views.bp)
+    app.register_blueprint(question_views.bp)
+    app.register_blueprint(answer_views.bp)
 
     return app
